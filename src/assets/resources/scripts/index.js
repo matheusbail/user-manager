380f08cbeb2ce54a0f2ea1763394e1fc5c6f3314
matheusbail <matheus312henrique@gmail.com>
2023-08-17 13:26:27 -0300

    start

---
+++ src/assets/resources/scripts/index.js
@@ -0,0 +1,12 @@
+function ToRegister()
+    {
+    window.location.href ="/src/app/register-user/register.html";
+    }
+function ToList(){
+    window.location.href ="/src/app/show-users/user-list.html";
+}
+
+function Previous()
+        {
+        window.history.back();
+        }
