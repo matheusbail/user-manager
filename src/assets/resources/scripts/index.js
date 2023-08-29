5f633cfb5d67cf455809b414e393d4bba5ccf5a5
matheusbail <matheus312henrique@gmail.com>
2023-08-29 14:31:11 -0300

    basic pages, start

--- src/assets/resources/scripts/index.js
+++ src/assets/resources/scripts/index.js
@@ -1,12 +1,11 @@
-function ToRegister()
-    {
-    window.location.href ="/src/app/register-user/register.html";
-    }
-function ToList(){
-    window.location.href ="/src/app/show-users/user-list.html";
+function ToRegister() {
+    window.location.href = "/src/app/register-user/register.html";
 }
 
-function Previous()
-        {
-        window.history.back();
-        }
+function ToList() {
+    window.location.href = "/src/app/show-users/user-list.html";
+}
+
+function Previous() {
+    window.history.back();
+}
