65fe9c460bf25962a98ea043d862ecc0cb8ed3f7
matheusbail <matheus312henrique@gmail.com>
2023-10-19 10:06:02 -0300

    client side validations on form

--- src/app/register-user/register.js
+++ src/app/register-user/register.js
@@ -1,19 +1,94 @@
-const Form = document.getElementById("form-register");
-const Name = document.getElementById("nome");
-const Mail = document.getElementById("email");
-const Cpf = document.getElementById("cpf");
-const Cep = document.getElementById("cep");
-const Fone = document.getElementById("fone");
-const GenM = document.getElementById("generoMasc");
-const GenF = document.getElementById("generoFem");
+const Form = document.getElementById('form-register');
+const Name = document.getElementById('nome');
+const Mail = document.getElementById('email');
+const Cpf = document.getElementById('cpf');
+const Cep = document.getElementById('cep');
+const Fone = document.getElementById('fone');
+const GenM = document.getElementById('generoMasc');
+const GenF = document.getElementById('generoFem');
 
 
 
-Form.addEventListener("submit", function (onclick) {
+Form.addEventListener("submit", event => {
     event.preventDefault();
 
-    alert("A");
-})
+
+    validate();
+});
+
+const setError = (element, message) => {
+    const formGroup = element.parentElement;
+    const errorDisplay = formGroup.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    formGroup.classList.add('error');
+    formGroup.classList.remove('sucess');
+}
+
+const sucess = element => {
+    const formGroup = element.parentElement;
+    const errorDisplay = formGroup.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    formGroup.classList.add('sucess');
+    formGroup.classList.remove('error');
+}
+
+
+
+const validate = () => {
+    const NameValue = Name.value.trim();
+    const MailValue = Mail.value.trim();
+    /* const CpfValue = Cpf.value.trim();
+     const CepValue = Cep.value.trim();
+     const FoneValue = Fone.value.trim();
+     const GenMValue = GenM.value.trim();
+     const GenFValue = GenF.value.trim();*/
+
+    if (NameValue === '') {
+        setError(Name, 'Nome de usuário é necessário');
+    } else {
+        sucess(Name);
+    }
+
+    if (MailValue === '') {
+        setError(Mail, 'EMAILAALSDMLASJDLHFLGHADH');
+    } else {
+        sucess(Mail);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*Form.addEventListener("submit", (event) => {
     event.preventDefault();
