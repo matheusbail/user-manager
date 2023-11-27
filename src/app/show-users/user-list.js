bafb516b92a6a50e952e0ee1eb35c915f3b7a227
matheusbail <matheus312henrique@gmail.com>
2023-11-27 11:39:09 -0300

    VIA-CEP-API working

--- src/app/show-users/user-list.js
+++ src/app/show-users/user-list.js
@@ -1,6 +1,7 @@
 window.addEventListener("load", event => {
     event.preventDefault();
     readJson();
+    // toggleLoader();
     //readJsonF(user);
 })
 
@@ -18,7 +19,7 @@ fetch("http://localhost:3000/user?_page=1&_limit=1")
     .then((req) => req.json())
     .then((data) => readJsonF(data));
 
-function readJsonF(user) {
+function readJsonF(user) { // CONSULTA O ARQUIVO JSON E RETORNA OS DADOS
 
     const html = user.map(
         (dado) => `
@@ -33,3 +34,31 @@ function readJsonF(user) {
     );
     document.getElementById('infosY').innerHTML = html;
 }
+
+const toggleLoader = () => { // FAZ O LOAD APARECER MUDANDO A CLASSE DOS DIVS 
+    const fadeElement = document.querySelector("#fade");
+    const loaderElement = document.querySelector("#loader");
+
+    fadeElement.classList.toggle("hide");
+    loaderElement.classList.toggle("hide");
+}
+
+
+const checkCep = document.querySelector("#cep")
+
+
+cep.addEventListener("blur", (e) => {
+    let busca = cep.value.replace("-", "")
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'default'
+    }
+
+    fetch(`https://viacep.com.br/ws/${busca}/json/`, options)
+        .then(response => {
+            response.json()
+                .then(data => document.getElementById('cepOutput').innerHTML = data.localidade)
+        })
+        .catch(e => console.log('Erro'))
+})
