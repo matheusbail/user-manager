c550f352b95d0a24638b706dd6dd91eb52ff11b6
matheusbail <matheus312henrique@gmail.com>
2023-11-29 14:41:43 -0300

    js checks

--- src/app/register-user/register.js
+++ src/app/register-user/register.js
@@ -2,25 +2,15 @@ const Form = document.getElementById('form-register');
 const Name = document.getElementById('nome');
 const Mail = document.getElementById('email');
 
-/*const Cpf = document.getElementById('cpf');
-const Cep = document.getElementById('cep');
-const Fone = document.getElementById('fone');
-const GenM = document.getElementById('generoMasc');
-const GenF = document.getElementById('generoFem');
-*/
-
-
 
 Form.addEventListener("submit", event => {
     event.preventDefault();
     validate();
     saveLocal();
+
 });
 
-window.addEventListener("onclick", event => {
-    event.preventDefault();
 
-})
 
 const setError = (element, message) => {
     const formGroup = element.parentElement;
