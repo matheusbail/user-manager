6d3d187f4f1e3ec6522b821792a93b63f4408c1e
matheusbail <matheus312henrique@gmail.com>
2023-11-28 21:56:44 -0300

    coments

--- src/app/show-users/user-list.js
+++ src/app/show-users/user-list.js
@@ -6,22 +6,17 @@ window.addEventListener("load", event => {
 })
 
 function readJson() {
-    //  let string = localStorage.getItem('data');
-    //let jsonString = JSON.stringify(string);
-    //const dados = document.getElementById('infos');
-    //dados.innerHTML = JSON.parse(jsonString);
-
-    let string = localStorage.getItem('data');
-    let jsonString = JSON.parse(string);
-    const dados = document.getElementById('infos');
-    dados.innerHTML = ` 
+    let string = localStorage.getItem('data'); // recupera o json q estava em string
+    let jsonString = JSON.parse(string); // trasnforma novamente em objeto
+    const dados = document.getElementById('infos'); // seleciona o id que sera manipulado 
+    dados.innerHTML = `  
     <p>${jsonString.nome}</p>
     <p>${jsonString.email}</p>
     <p>${jsonString.cpf}</p>
     <p>${jsonString.cep}</p>
     <p>${jsonString.fone}</p>
     <p>${jsonString.cargo}</p>
-    `;
+    `; // insere dados no campo selecionado
 }
 
 fetch("http://localhost:3000/user?_page=1&_limit=1")
@@ -53,13 +48,13 @@ const toggleLoader = () => { // FAZ O LOAD APARECER MUDANDO A CLASSE DOS DIVS
 }
 
 
-const checkCep = document.querySelector("#cep")
+const checkCep = document.querySelector("#cep") // seleciona o span com a id cep para o evento de blur ser escutado
 
 
-cep.addEventListener("blur", (e) => {
-    let busca = cep.value.replace("-", "")
+cep.addEventListener("blur", (e) => { // evento de blur para assim que for tirado atencao do input, seja por clique ou tab, dispare o evento
+    let busca = cep.value.replace("-", "") // busca o - no cep se for inserido e remove
     const options = {
-        method: 'GET',
+        method: 'GET', // metodo de pesquisa com o cep q foi informado
         mode: 'cors',
         cache: 'default'
     }
@@ -67,7 +62,7 @@ cep.addEventListener("blur", (e) => {
     fetch(`https://viacep.com.br/ws/${busca}/json/`, options) // crase evita o uso do + para concatenação, no caso de uma variavel de busca
         .then(response => {
             response.json()
-                .then(data => document.getElementById('cepOutput').innerHTML = data.localidade)
+                .then(data => document.getElementById('cepOutput').innerHTML = data.localidade) //resposta da API com os dados sendo inseridos na span
         })
-        .catch(e => console.log('Erro'))
+        .catch(e => alert('Erro, CEP Inválido :(')) //caso seja um cep inexistente exibe no alerta a mensagem de erro
 })
